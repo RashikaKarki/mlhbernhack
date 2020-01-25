dba753741fc0fbff3e761907c3c74b8bb34673ec
Ashish Subedi <ashishsubedi10@outlook.com>
2020-01-25 10:10:33 +0545

    Retrained model for greater accuracy

---
+++ cvc.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import os
+
+
+folders = ['processed_data/'+folder for folder in os.listdir('processed_data')]
+
+new_folders = ['processed_data_2/'+folder for folder in os.listdir('processed_data')]
+for i,folder in enumerate(folders):
+    for f in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,f))
+        # print(img.shape)
+        img = np.resize(img,(128,128))
+        
+
+        if(not os.path.exists(new_folders[i])):
+            os.mkdir(new_folders[i])
+        cv2.imwrite(os.path.join(new_folders[i],f),img)
+        print(f,"Saved")
+
+# f = "C:\\Users\\Dell\\Desktop\\mlhbernhack\\test.jpg"
+# img = cv2.imread(os.path.join(f))
+# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# edges = cv2.Canny(gray,10,250)
+# cv2.imshow("widnow",edges)
+# cv2.imwrite('test_edge.jpg',edges)
+# # if(not os.path.exists(new_folders[i])):
+#     # os.mkdir(new_folders[i])
+# # cv2.imwrite(os.path.join(new_folders[i],f),gray)
+# # print(f,"Saved")
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
