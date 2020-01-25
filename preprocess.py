cebac0406c91e921f4b2322c08a44f75b5e4d1fe
Ashish Subedi <ashishsubedi10@outlook.com>
2020-01-25 06:14:42 +0545

    Bug FIxed

--- preprocess.py
+++ preprocess.py
@@ -7,42 +7,41 @@ import tensorflow as tf
 import base64
 import json
 
-labels = [f for f in os.listdir('processed_data')]
+# labels = [f for f in os.listdir('processed_data')]
+labels = ['none','character_01_ka','character_02_kha']
 
 
-with open('myjsonfile.json') as f:
-  data = json.load(f)
+# with open('myjsonfile.json') as f:
+#   data = json.load(f)
 
-# data = str(data)
-data = data['image']
+# # data = str(data)
+# data = data['image']
 
-# print(data)
-# img_data = base64.b64decode(data)
-# print(img_data)
-
-with open("tmp.jpg", "wb") as fh:
-    # res = base64.decodebytes(img_data)
-    fh.write(base64.decodebytes(data.encode()))
+# # print(data)
 
 
-model = tf.keras.models.load_model('my_model.h5');
+# with open("tmp.jpg", "wb") as fh:
+#     fh.write(base64.decodebytes(data.encode()))
 
+
+model = tf.keras.models.load_model('model_5.h5');
+print(model.summary())
 img = cv2.imread('tmp.jpg');
-# # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-# edges = cv2.Canny(gray,30,120)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,30,120)
 # cv2.imshow("widnow",edges)
-# img = cv2.resize(edges,(32,32))
-# img = np.array(img).astype('float32')
-# img = np.reshape(img,(1,64,64,1))
+img = cv2.resize(edges,(64,64))
+img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+img = np.array(img).astype('float32')
+img = np.reshape(img,(1,64,64,3))
 
 
-img = cv2.resize(img,(32,32))
 
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edge = cv2.Canny(gray,30,120)
-# cv2.imshow("Windwo",img)
-img = np.reshape(edge,(1,32,32,1)).astype('float32')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# edge = cv2.Canny(gray,30,120)
+# cv2.imshow("Windwo",edge)
+# img = np.reshape(edge,(1,32,32,1)).astype('float32')
 
 #Send image to predict
 # cv2.waitKey(0)
